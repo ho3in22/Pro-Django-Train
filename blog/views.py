ba3ca0b2eaec7d5e774c3c555b8c574ec8fcbfbe
ho3in22 <hosseinkordamiri@gmail.com>
2026-01-05 19:47:10 +0330

    Make blog posts dynamic and update blog URLs
    Replaced static blog content with dynamic data from the Post model in both blog-home and blog-single templates. Updated blog views to fetch posts from the database and pass them to templates. Changed the blog single URL to accept a post ID parameter and adjusted navigation and links accordingly. Simplified the main navigation menu in base.html.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
+from blog.models import *
 
 # Create your views here.
 def blog_view(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts' : posts}
+    return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request, pid):
+    # posts = Post.objects.filter(status=1)
+    # post = get_object_or_404(posts,pk = pid)
+
+    post = get_object_or_404(Post,pk = pid, status = 1)
+    context = {'post' : post}
+    return render(request, 'blog/blog-single.html', context)
